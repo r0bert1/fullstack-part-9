62b203874f743a450f543d80d8d24a99ec8edc5c
r0bert1 <rpakkane@gmail.com>
2021-11-16 14:36:01 +0200

    Added error handling to bmiCalculator

--- 9.1.-9.7./bmiCalculator.ts
+++ 9.1.-9.7./bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface Arguments {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): Arguments => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   const bmi: number = weight / (height / 100) ** 2;
   if (bmi < 18.5) return "Underweight";
@@ -6,10 +25,18 @@ const calculateBmi = (height: number, weight: number): string => {
   return "Normal";
 };
 
-/* 
+/*
   Example script with command line arguments:
   npm run calculateBmi 180 91
 */
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
-console.log(calculateBmi(height, weight));
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong.";
+  if (error instanceof Error) {
+    errorMessage = " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
