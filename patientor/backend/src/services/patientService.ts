b43a520cc0292927ace843e3807d65e025b95ca4
r0bert1 <rpakkane@gmail.com>
2022-04-10 14:31:39 +0300

    Exercise 9.19

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,9 +1,9 @@
 import { v1 as uuid } from "uuid";
 
-import patientsData from "../../data/patients.json";
+import patientsData from "../../data/patients";
 import { NonSensitivePatient, NewPatient, Patient } from "../types";
 
-const patients: Array<Patient> = patientsData as Array<Patient>;
+const patients: Array<Patient> = patientsData;
 
 export const getNonSensitivePatients = (): NonSensitivePatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
@@ -17,14 +17,11 @@ export const getNonSensitivePatients = (): NonSensitivePatient[] => {
 
 export const getPatientById = (id: string): Patient | undefined => {
   const patient = patients.find((patient) => patient.id === id);
-  if (patient && !patient.entries) patient.entries = [];
   return patient;
 };
 
-export const addPatient = (entry: NewPatient): NonSensitivePatient => {
-  const newPatient = { id: uuid(), ...entry };
+export const addPatient = (entry: NewPatient): Patient => {
+  const newPatient = { id: uuid(), entries: [], ...entry };
   patients.push(newPatient);
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  const { ssn, ...nonSensitivePatient } = newPatient;
-  return nonSensitivePatient;
+  return newPatient;
 };
